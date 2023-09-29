42d83aa5c392cc5a0ebb0339e02a04195a7b87e5
Fabio <fabio.gromagnoli@gmail.com>
2023-09-29 15:00:11 +0100

    update tab.json

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("brasileirao.controller.Main", {
             onInit: function () {
+                
+                let dadosGerais = {
+                    nomeCampeonato : "Brasileirao 2023 no Canal FioriNET.",
+                    rodada : 99
+                }
 
-            }
-        });
-    });
+                let dadosModel =  new JSONModel();
+                dadosModel.setData(dadosGerais)
+                let tela = this.getView()
+                tela.setModel(dadosModel, "ModeloDadosGerais")
+
+                }
+        })
+    })
